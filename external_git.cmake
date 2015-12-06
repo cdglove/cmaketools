b774e5481135493e5f3eae05c5969ca5a56f128c
Chris Glover <c.d.glover@gmail.com>
2015-12-06 12:45:35 -0500

    Fix a few bugs in external git repo management.

--- external_git.cmake
+++ external_git.cmake
@@ -18,11 +18,10 @@ include (CMakeParseArguments)
 #
 ###############################################################################
 function(clone_external_git_repo)
-  set(options ALWAYS_UPDATE TAG)
-  set(oneValueArgs URL TARGET_DIR)
-  cmake_parse_arguments(clone_external "${options}" "${oneValueArgs}" "" ${ARGN} )
+  set(oneValueArgs URL TARGET_DIR TAG ALWAYS_UPDATE)
+  cmake_parse_arguments(clone_external "" "${oneValueArgs}" "" ${ARGN} )
 
-  if(NOT EXISTS "${clone_external_TARGET_DIR}/_.git")
+  if(NOT EXISTS "${clone_external_TARGET_DIR}/.git")
     message(STATUS "Cloning repo ${clone_external_URL}")
     execute_process(
       COMMAND "${GIT_EXECUTABLE}" clone ${clone_external_URL} --branch ${clone_external_TAG} ${clone_external_TARGET_DIR}
@@ -55,19 +54,19 @@ endfunction(clone_external_git_repo)
 #
 ###############################################################################
 function(add_external_git_repo)
-  set(options ALWAYS_UPDATE TAG ADD_SUBDIR)
-  set(oneValueArgs URL PREFIX)
+  set(options ALWAYS_UPDATE ADD_SUBDIR)
+  set(oneValueArgs URL PREFIX TAG)
   cmake_parse_arguments(external_git "${options}" "${oneValueArgs}" "" ${ARGN} )
 
   clone_external_git_repo(
     URL ${external_git_URL} 
     TAG ${external_git_TAG} 
-    PREFIX "${PROJECT_SOURCE_DIR}/${external_git_PREFIX}"
+    TARGET_DIR "${PROJECT_SOURCE_DIR}/${external_git_PREFIX}"
     ALWAYS_UPDATE ${external_git_ALWAYS_UPDATE})
 
   get_filename_component(full_path_source_dir "${PROJECT_SOURCE_DIR}/${external_git_PREFIX}" ABSOLUTE)
   get_filename_component(full_path_bin_dir "${PROJECT_BINARY_DIR}/${external_git_PREFIX}" ABSOLUTE)
-  if(${ADD_SUBDIR})
+  if(${external_git_ADD_SUBDIR})
     add_subdirectory(${full_path_source_dir} ${full_path_bin_dir})
   endif()
 endfunction(add_external_git_repo)
